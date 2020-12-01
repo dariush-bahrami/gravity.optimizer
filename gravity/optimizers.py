26493727ea3aa536003b23efcb9298de7f27c6ec
dAriush <dariushbahrami1993@gmail.com>
2020-12-01 17:13:23 +0330

    beta as a function of t

--- gravity/optimizers.py
+++ gravity/optimizers.py
@@ -3,50 +3,49 @@ import tensorflow as tf
 
 
 class Gravity(tf.keras.optimizers.Optimizer):
-    def __init__(self, learning_rate, beta=0.9, name="Gravity", **kwargs):
+    def __init__(self, learning_rate=0.01, alpha=4e-3, beta=0.9, name="Gravity", **kwargs):
         super(Gravity, self).__init__(name, **kwargs)
         self._set_hyper('learning_rate', kwargs.get('lr', learning_rate))
         self._set_hyper('decay', self._initial_decay)
+        self._set_hyper('alpha', alpha)
         self._set_hyper('beta', beta)
         self.epsilon = 1e-7
-
     def _create_slots(self, var_list):
+        alpha = self._get_hyper("alpha")
+        stddev = alpha/self.learning_rate
+        initializer = tf.keras.initializers.TruncatedNormal(mean=0.0, stddev=stddev, seed=None)
         for var in var_list:
-            self.add_slot(var, "velocity")
+            self.add_slot(var, "velocity", initializer=initializer)
 
     @tf.function
     def _resource_apply_dense(self, grad, var):
         # Get Data
         var_dtype = var.dtype.base_dtype
-        lr_t = self._decayed_lr(var_dtype)  # handle learning rate decay
+        lr_t = self._decayed_lr(var_dtype) # handle learning rate decay
         beta = self._get_hyper("beta", var_dtype)
         velocity = self.get_slot(var, "velocity")
 
         # Calculations
-        max_step_grad = 1 / (tf.reduce_max(tf.abs(grad)) + self.epsilon)
-        gradient_term = grad / (1 + (grad / (max_step_grad + self.epsilon))**2)
+        max_grad = tf.math.reduce_max(grad)
+        min_grad = tf.math.reduce_min(grad)
+        max_step_grad = 1 / (max_grad-min_grad+self.epsilon)
+        gradient_term = grad / (1 + (grad/(max_step_grad+self.epsilon))**2)
 
         # update variables
-        updated_velocity = velocity.assign(beta * velocity +
-                                           (1 - beta) * gradient_term)
-        updated_var = var.assign(var - lr_t * updated_velocity)
-
+        updated_velocity = velocity.assign(beta*velocity + (1-beta)*gradient_term) 
+        updated_var = var.assign(var - lr_t*updated_velocity)       
+        
         # updates = [updated_var, updated_velocity]
         # return tf.group(*updates)
     def _resource_apply_sparse(self, grad, var):
         raise NotImplementedError
-
     def get_config(self):
         config = super(Gravity, self).get_config()
         config.update({
-            'learning_rate':
-            self._serialize_hyperparameter('learning_rate'),
-            'decay':
-            self._serialize_hyperparameter('decay'),
-            'beta':
-            self._serialize_hyperparameter('beta'),
-            'epsilon':
-            self.epsilon,
+            'learning_rate': self._serialize_hyperparameter('learning_rate'),
+            'decay': self._serialize_hyperparameter('decay'),
+            'beta': self._serialize_hyperparameter('beta'),
+            'epsilon': self.epsilon,
         })
         return config
 
